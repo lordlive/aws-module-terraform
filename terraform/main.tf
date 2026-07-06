c4e4a09d46660f9a47990e95ab93265be547ad11
Oleh Zhuravel <lordlive74@gmail.com>
2026-07-06 12:48:31 +0300

    update network setting in the EKS

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,8 +58,8 @@ module "eks" {
   # iam_role_arn_node              = var.iam_role_arn_node
 
   # Network settings - Switcher: if switcher_default_vpc = false, use existing IDs, if true use IDs from VPC module
-  vpc_id          = module.vpc[dev].vpc_id
-  private_subnets = module.vpc[dev].aws_subnets.private_subnets.ids
+  vpc_id          = module.vpc[each.value].vpc_id
+  private_subnets = module.vpc[each.value].private_subnets
 
   # Security groups configuration
   create_node_security_group = var.create_node_security_group
