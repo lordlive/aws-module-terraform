c9a4f556205c5649ba21ef082133350638f73cc5
Oleh Zhuravel <lordlive74@gmail.com>
2026-07-07 11:28:51 +0300

    recreate s3

--- terraform/modules/s3/s3.tf
+++ terraform/modules/s3/s3.tf
@@ -2,17 +2,12 @@ module "s3" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "3.0.1"
 
-  bucket = "${var.bucket_name}-${var.environment}"
-
+  bucket                  = var.bucket_name
   versioning              = var.versioning
   block_public_acls       = var.block_public_acls
   block_public_policy     = var.block_public_policy
   ignore_public_acls      = var.ignore_public_acls
   restrict_public_buckets = var.restrict_public_buckets
-
-  tags = {
-    Environment = var.environment
-  }
 }
 
 resource "aws_s3_object" "s3_folders" {
