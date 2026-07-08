e4ab0085bdac9c41d172efd932145a9c10f5a60e
Oleh Zhuravel <lordlive74@gmail.com>
2026-07-08 14:41:26 +0300

    disable outputs

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -6,12 +6,12 @@ output "name" {
   value = aws_acm_certificate.test_cert.arn
 } */
 
-output "target_group_arn" {
-  description = "ARN of the ALB target group"
-  value       = module.alb.target_group_arns[0]
-}
+# output "target_group_arn" {
+#   description = "ARN of the ALB target group"
+#   value       = module.alb.target_group_arns[0]
+# }
 
-output "target_group_name" {
-  description = "Name of the ALB target group"
-  value       = module.alb.target_group_names[0]
-}
+# output "target_group_name" {
+#   description = "Name of the ALB target group"
+#   value       = module.alb.target_group_names[0]
+# }
